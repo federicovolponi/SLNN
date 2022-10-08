6cf10136cde1536d263303c9c0cdd3dc82de2070
Federico Volponi <102225576+federicovolponi@users.noreply.github.com>
2022-10-08 20:54:42 +0200

    Update es01.py

--- LAB01/es01.py
+++ LAB01/es01.py
@@ -22,6 +22,7 @@ accuracy_test = []
 accuracy_train = []
 prediction_test = 0
 prediction_train = 0
+n_features = 2
 #Function for calculating euclidean distances (2 dimensions)
 def euclidean_distance(p, q):
   dist = np.sqrt(np.sum(np.square(p - q)))
@@ -34,28 +35,28 @@ def K_NNClassifier(sample, k):
   neighbors_index = []
   #Create a vector with all the distances
   for i in range(len(Train_Set)):
-    dist.append(euclidean_distance(sample, Train_Set[i, :2]))
+    dist.append(euclidean_distance(sample, Train_Set[i, :n_features]))
 
   neighbors_index = np.argsort(dist)
   for i in range(k):
-    neighbors.append(Train_Set[neighbors_index[i],2]) #Vector containing the classes of the k-nearest elements
+    neighbors.append(Train_Set[neighbors_index[i],n_features]) #Vector containing the classes of the k-nearest elements
   
   classes,counts = np.unique(neighbors, return_counts=True) #Evaluate the majority class
   return classes[np.argmax(counts)]
 
 #Calculate correct prediction for test and train sets 
-for k in range(1,200,9):
+for k in range(1,200,2):
   prediction_test = 0
   prediction_train = 0
   for i in range(len(Test_Set)):
-    if K_NNClassifier(Test_Set[i, :2], k) == Test_Set[i, 2]:
+    if K_NNClassifier(Test_Set[i, :n_features], k) == Test_Set[i, n_features]:
       prediction_test += 1
-    if K_NNClassifier(Train_Set[i, :2], k) == Train_Set[i, 2]:
+    if K_NNClassifier(Train_Set[i, :n_features], k) == Train_Set[i, n_features]:
       prediction_train += 1
   accuracy_test.append(prediction_test/len(Test_Set))
   accuracy_train.append(prediction_train/len(Train_Set))
 
-k = range(1,200, 9)
+k = range(1,200, 2)
 fig, ax = plt.subplots()
 ax.plot(k, accuracy_test, "b", label="Test accuracy")
 ax.plot(k, accuracy_train, "r", label="Train accuracy")
